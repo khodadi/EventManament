723e9c0314731c77ef373c12167b8db966e50201
k.khodadi <Qmars@123>
2023-07-22 00:12:00 +0330

    reorganize project

---
+++ Eureka/src/main/java/com/AppServiceRecovery.java
@@ -0,0 +1,22 @@
+package com;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+/**
+ * @Creator 5/7/2023
+ * @Project IntelliJ IDEA
+ * @Author k.khodadi
+ **/
+
+
+@EnableEurekaServer
+@SpringBootApplication
+public class AppServiceRecovery {
+    public static void main(String[] args) {
+        SpringApplication.run(AppServiceRecovery.class, args);
+    }
+
+}
+
--- EventSolution/Notification/src/main/java/com/notification/repo/irepo/IProvinceRepository.java
+++
@@ -1,9 +0,0 @@
-package com.notification.repo.irepo;
-
-import com.notification.entity.Province;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface IProvinceRepository extends JpaRepository<Province,Long> {
-}
